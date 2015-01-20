9443c3d348e8a2887ee91b714b324362392ab136
Jason Harris <jth@mibot.com>
2015-01-20 08:12:41 -0800

    Adding parser actions.

--- gcode/gcode_parser.py
+++ gcode/gcode_parser.py
@@ -2,9 +2,8 @@
 
 ifile = 'test.gc'
 
-
 #------------------------------------------------------------------------------
-# lexer
+# scanner
 
 import ply.lex as lex
 
@@ -80,16 +79,21 @@ def p_line(p):
   '''line : line_number
           | line_number words
           | words'''
-  pass
+  print('p_line')
 
 def p_words(p):
   '''words : words word
            | word'''
-  pass
+  if len(p) == 3:
+    p[0] = '%s %s' % (p[1], p[2])
+  else:
+    p[0] = p[1]
+  print p[0]
 
 def p_line_number(p):
-  '''line_number : N INTEGER'''
-  print('%s%s' % (p[1], p[2]))
+  'line_number : N INTEGER'
+  #print('%s%s' % (p[1], p[2]))
+  pass
 
 def p_word(p):
   '''word : general_function
@@ -98,60 +102,106 @@ def p_word(p):
           | feed_rate
           | spindle_speed
           | tool_length_offset_index
+          | arc_radius
+          | feed_increment
+          | dwell_time
+          | x_offset
+          | y_offset
+          | z_offset
           | x_axis
           | y_axis
           | z_axis
           | a_axis
           | b_axis
-          | c_axis'''
+          | c_axis
+          | u_axis
+          | v_axis
+          | w_axis'''
+  p[0] = p[1]
 
 def p_general_function(p):
   'general_function : G INTEGER'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_tool_selection(p):
   'tool_selection : T INTEGER'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_misc_function(p):
   'misc_function : M INTEGER'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_feed_rate(p):
   'feed_rate : F DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_spindle_speed(p):
   'spindle_speed : S INTEGER'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_tool_length_offset_index(p):
   'tool_length_offset_index : H INTEGER'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_x_axis(p):
   'x_axis : X DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_y_axis(p):
   'y_axis : Y DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_z_axis(p):
   'z_axis : Z DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_a_axis(p):
   'a_axis : A DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_b_axis(p):
   'b_axis : B DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
 
 def p_c_axis(p):
   'c_axis : C DECIMAL'
-  print('%s%s' % (p[1], p[2]))
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_u_axis(p):
+  'u_axis : U DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_v_axis(p):
+  'v_axis : V DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_w_axis(p):
+  'w_axis : W DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_x_offset(p):
+  'x_offset : I DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_y_offset(p):
+  'y_offset : J DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_z_offset(p):
+  'z_offset : K DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_dwell_time(p):
+  'dwell_time : P DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_feed_increment(p):
+  'feed_increment : Q DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
+
+def p_arc_radius(p):
+  'arc_radius : R DECIMAL'
+  p[0] = '%s%s' % (p[1], p[2])
 
 # Error rule for syntax errors
 def p_error(p):
@@ -162,21 +212,23 @@ def p_error(p):
 parser = yacc.yacc()
 
 #------------------------------------------------------------------------------
+# scan for tokens (debug)
+
+def scan(x):
+  lexer.input(x)
+  while True:
+    tok = lexer.token()
+    if not tok:
+      break
+    print tok
+
+#------------------------------------------------------------------------------
 
 def main():
   f = open(ifile, 'r')
   x = f.read()
   f.close()
-
-  #lexer.input(x)
-
-  #while True:
-    #tok = lexer.token()
-    #if not tok: break      # No more input
-    #print tok
-
-  result = parser.parse(x)
-  print result
+  parser.parse(x)
 
 main()
 
